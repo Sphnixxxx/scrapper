211c3c82738ad2b3d4f95b05d27606654320d661
Sphnixxxx <sagarbdrchand01@gmail.com>
2025-11-26 13:51:31 +0545

    finish project

---
+++ scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+url ="https://books.toscrape.com/"
+def scrape_books(url):
+    response=requests.get(url)
+    if response.status_code!=200:
+        return
+    # handle all special character
+    response.encoding=response.apparent_encoding
+    soup=BeautifulSoup(response.text,"html.parser")
+    books=soup.find_all("article",class_="product_pod")
+    all_books=[]
+    for book in books:
+        title=book.h3.a['title']
+        price_text=book.find("p",class_='price_color').text
+        currency=price_text[0]
+        price=float(price_text[1:])
+        all_books.append(
+            {
+                "title":title,
+                "price":price,
+                "currency":currency
+            }
+        )
+    return all_books
+books=scrape_books(url)
+# print(books)
+
+
+with open("books.json","w",encoding="utf-8") as f:
+    import json
+    json.dump(books,f,indent=4,ensure_ascii=False)
+    
